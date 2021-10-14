ee2e5ae75b3517a4cb038535a1dfd89535f71964
Shohrat-Code <shohrat@code.edu.az>
2021-10-14 15:45:39 +0400

    Codes

---
+++ Algorithms-2/Algorithms-2/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace Algorithms_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int num1, num2, sum = 0;
+            //Console.WriteLine("Enter number 1:");
+            //num1 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Enter number 2:");
+            //num2 = Convert.ToInt32(Console.ReadLine());
+
+            //sum = num1 + num2;
+            //Console.WriteLine("Sum of two numbers is " + sum);
+
+            //Console.WriteLine("Enter the number:");
+            //int number = Convert.ToInt32(Console.ReadLine());
+
+            //if (number%3==0)
+            //{
+            //    Console.WriteLine("Eded 3-e tam bolunur.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Eded 3-e tam bolunmur.");
+            //}
+
+
+            //Console.WriteLine("Enter the number:");
+            //int number = Convert.ToInt32(Console.ReadLine());
+
+            //if (number > 0)
+            //{
+            //    Console.WriteLine("Eded musbetdir.");
+            //}
+            //else if (number == 0)
+            //{
+            //    Console.WriteLine("Eded sifirdir.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Eded menfidir.");
+            //}
+
+            //Console.WriteLine("Please enter the season:");
+            //string season = Console.ReadLine();
+
+            //switch (season)
+            //{
+            //    case "spring":
+            //        Console.WriteLine("Spring");
+            //        break;
+            //    case "summer":
+            //        Console.WriteLine("Summer");
+            //        break;
+            //    case "autumn":
+            //        Console.WriteLine("Autumn");
+            //        break;
+            //    case "winter":
+            //        Console.WriteLine("Winter");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Enter currect season!");
+            //        break;
+            //}
+
+            //for (int i = 1; i <= 4; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+            //int i = 1;
+            //while (i != 0)
+            //{
+            //    Console.WriteLine("0. Exit");
+            //    Console.WriteLine("1. Continue");
+            //    Console.WriteLine();
+
+            //    i = Convert.ToInt32(Console.ReadLine());
+            //    Console.WriteLine("Salam");
+            //}
+
+
+            //int i;
+            //do
+            //{
+            //    Console.WriteLine("0. Exit");
+            //    Console.WriteLine("1. Continue");
+            //    i = Convert.ToInt32(Console.ReadLine());
+            //    Console.WriteLine("Salam");
+            //    Console.WriteLine();
+            //} while (i != 0);
+
+        }
+    }
+}
